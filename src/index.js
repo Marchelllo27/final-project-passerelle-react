e4ff8ffe91c12508b0b00b3d43cfc490fac27e60
Marc Tofan <tofmana@gmail.com>
2022-02-07 16:36:17 +0100

    inital work on the app

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <StyledEngineProvider injectFirst>
     <App />
-  </StyledEngineProvider>,
+  </StyledEngineProvider>
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/shared/components/MainHeader.jsx
+++ src/shared/components/MainHeader.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
+import DinnerDiningIcon from "@mui/icons-material/DinnerDining";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 import Navigation from "./Navigation";
-import classes from "./MainHeader.module.css"
+import Busket from "./Busket";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = props => {
-  return <header className={classes.mainHeader}>
-    <h1>LOGO</h1>
-    <Navigation />
-  </header>;
+  return (
+    <header className={classes.mainHeader}>
+      <Link className={classes.logoBox} to="/">
+        <DinnerDiningIcon className={classes.logo} />
+      </Link>
+      <nav className="control-account">
+      <Navigation />
+        <Busket classes={classes.basketIcon} />
+        <AccountCircleIcon fontSize="large" />
+      </nav>
+    </header>
+  );
 };
 
 export default MainHeader;
