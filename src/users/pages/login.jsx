d6de139c5bbeb128e65487685f0a5a4bbeab6d2c
hibabeb <hbb.devpro.com>
2022-02-07 17:03:30 +0100

    edit Signup and login

--- src/users/pages/login.jsx
+++ src/users/pages/login.jsx
@@ -1,5 +1,92 @@
-const Login = props => {
-  return <p>LOGIN</p>;
-};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Button, Link, Card,CardContent ,OutlinedInput,InputLabel,TextField,FormControl,InputAdornment} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
 
+import FormHelperText from "@mui/material/FormHelperText";
+
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+
+
+const Login = (props) => {
+  const [values, setValues] = React.useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  
+
+  return (
+    <Card >
+      <CardContent>
+        <Box sx={{ display: "flex", mx: "auto", width: 200 }}>
+          <div>
+            <TextField
+              label="Entrez votre Email"
+              id="email"
+              sx={{ m: 1, width: "35ch" }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Email : </InputAdornment>
+                ),
+              }}
+            />
+
+            <FormControl
+              sx={{ m: 1, width: "35ch", display: "flex" }}
+              variant="outlined"
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <Button>Se connecter</Button>
+          </div>
+        </Box>
+
+        <div>
+          <Link path="/signup">Vous n'avez pas encore de compte ?</Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 export default Login;
--- src/users/pages/signup.jsx
+++ src/users/pages/signup.jsx
@@ -1,5 +1,62 @@
-const Signup = props => {
-  return <p>SIGN-UP</p>;
+import { TextField, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+// import classes from "./Signup.module.css";
+ import { spacing } from "@mui/system";
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: spacing(20),
+    "& .MuiTextField-root": {
+      margin: spacing(11),
+      width: "300px",
+    },
+    "& .MuiButtonBase-root": {
+      margin: spacing(20),
+    },
+  },
+});
+const Signup = (props) => {
+  const classes = useStyles();
+  
+  return (
+    <form  className={classes.root}>
+      <TextField
+        //sx={{ paddingTop: 2 }}
+        label="Votre prénom"
+        variant="filled"
+        required
+      />
+      <TextField label="Votre nom" variant="filled" required />
+      <TextField label="Votre email" variant="filled" type="email" required />
+
+      <TextField label="Votre numéro de téléphone" variant="filled" required />
+      <p>Votre adresse</p>
+      <TextField label="Rue" variant="filled" required />
+      <TextField label="Code Postal" variant="filled" type="number" required />
+      <TextField label="Ville" variant="filled" required />
+
+      <TextField
+        label="Mot de passe"
+        variant="filled"
+        type="password"
+        required
+      />
+      <TextField
+        label="confirmer votre Mot de passe"
+        variant="filled"
+        type="Password"
+        required
+      />
+      <div>
+        <Button type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default Signup;
