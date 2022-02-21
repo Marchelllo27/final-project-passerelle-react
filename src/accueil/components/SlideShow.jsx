056552051eeb1fd8067c85b49d3b01d12b598d3e
Marc Tofan <tofmana@gmail.com>
2022-02-21 21:09:55 +0100

    little changesssss

--- src/accueil/components/SlideShow.jsx
+++ src/accueil/components/SlideShow.jsx
@@ -1,36 +1,36 @@
-// import React from "react";
-// import {Slide} from "react-slideshow-image";
+import React from "react";
+import {Slide} from "react-slideshow-image";
 
-// import "react-slideshow-image/dist/styles.css";
-// import classes from "./SlideShow.module.css";
+import "react-slideshow-image/dist/styles.css";
+import classes from "./SlideShow.module.css";
 
-// const slideImages = [
-//   {
-//     url: "/salade-concombres.jpeg",
-//     caption: "Nos Plats",
-//   },
-//   {
-//     url: "/opera.jpg",
-//     caption: "Nos Desserts",
-//   },
-//   {
-//     url: "/pamplemousse.jpeg",
-//     caption: "Nos Boissons",
-//   },
-// ];
+const slideImages = [
+  {
+    url: "/salade-concombres.jpeg",
+    caption: "Nos Plats",
+  },
+  {
+    url: "/opera.jpg",
+    caption: "Nos Desserts",
+  },
+  {
+    url: "/pamplemousse.jpeg",
+    caption: "Nos Boissons",
+  },
+];
 
-// const Slideshow = () => {
-//   return (
-//     <div className={classes.containerSlide}>
-//       <Slide>
-//         {slideImages.map((slideImage, index) => (
-//           <div className={classes.eachSlide} key={index}>
-//               <img src={slideImage.url} alt="dish" />
-//           </div>
-//         ))}
-//       </Slide>
-//     </div>
-//   );
-// };
+const Slideshow = () => {
+  return (
+    <div className={classes.containerSlide}>
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div className={classes.eachSlide} key={index}>
+              <img src={slideImage.url} alt="dish" />
+          </div>
+        ))}
+      </Slide>
+    </div>
+  );
+};
 
-// export default Slideshow;
+export default Slideshow;
